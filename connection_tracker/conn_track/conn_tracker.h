2d0d7a5dacbee9e4b9a1dee51ddbd3fcdefda6ab
Alan Jowett <alan.jowett@microsoft.com>
2022-05-13 16:28:26 -0600

    Add conn_track demo for native code generation
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

---
+++ connection_tracker/conn_track/conn_tracker.h
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "net/ip.h"
+
+typedef struct _ip_address
+{
+    union
+    {
+        uint32_t ipv4;
+        uint32_t ipv6[4];
+    };
+} ip_address_t;
+
+typedef struct _connection_tuple
+{
+    ip_address_t src_ip;
+    uint16_t src_port;
+    ip_address_t dst_ip;
+    uint16_t dst_port;
+    uint32_t protocol;
+} connection_tuple_t;
